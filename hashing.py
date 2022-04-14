42a76006eff6f851fa586ab05187214dcd6db09a
maxblenner <maxb@live.ie>
2022-04-14 16:10:06 +0100

    put hashing and checking into functions

--- hashing.py
+++ hashing.py
@@ -3,9 +3,20 @@
 from encodings import utf_8
 import bcrypt
 
-userIn = input(b"Enter something to be hashed: ").encode('utf_8') #bcrypt requires byte strings and bcrypt.hashpw requires utf_8 encoding
+def hashPassword(userIn):
+    hashedPW = bcrypt.hashpw(userIn,bcrypt.gensalt())
+    return hashedPW
 
-hashedPW = bcrypt.hashpw(userIn,bcrypt.gensalt())
+def checkPassword(userIn): 
+    if bcrypt.checkpw(userIn,hashedPW):
+        print("Correct Password")
+    else:
+        print("Incorrect Password")
+
+userIn = input(b"Enter something to be hashed: ").encode('utf_8') #bcrypt requires byte strings and bcrypt.hashpw requires utf_8 encoding
+hashedPW = hashPassword(userIn)
+
+checkPassword(userIn)
 
 print(hashedPW)
 
