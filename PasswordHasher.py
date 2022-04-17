09b87fd499c771a11cb6b09c2ea54f5986f906cf
maxblenner <maxb@live.ie>
2022-04-17 21:45:45 +0100

    update key functionality

--- PasswordHasher.py
+++ PasswordHasher.py
@@ -6,16 +6,22 @@ def create_key():
         f.write(key)
     
 def load_key():
-    with open("userKey.txt", 'rb') as f:
-        key = f.read()
-        return key
-
-def encrypt_password(key, password):
+    try: 
+        with open("userKey.txt", 'rb') as f:
+            key = f.read()
+            return key
+    except: 
+        print("No local key file found!, key created")
+        return None
+    
+def encrypt_password(password):
+    key = load_key()
     f = Fernet(key)
     encrypted = f.encrypt(password)
     return(encrypted)
 
-def decrypt_password(key, encrypted_pw):
+def decrypt_password(encrypted_pw):
+    key = load_key()
     f = Fernet(key)
     decrypted = f.decrypt(encrypted_pw)
     return(decrypted)
@@ -25,15 +31,18 @@ new_user = True
 if new_user == True: 
     key = None
 
-if key == None: 
+if load_key() == None: 
     create_key()
 
-key = load_key()
+#key = load_key()
 
 #arbitrary code to test if functions are working
 userIn = input("Enter something to be hashed: ")
-encrypted = encrypt_password(key, userIn.encode())
+encrypted = encrypt_password(userIn.encode())
 print(encrypted)
-print(decrypt_password(key,encrypted))
+print(decrypt_password(encrypted))
 
 
+#7DXGEcBBW304_l61kLB8wCpauWoRzoMhAkXe82I4pTM=
+#ppsfSihZqVGtU-xnry4eMQ8P34n0-QatwrkGUfLOHrE=
+#ppsfSihZqVGtU-xnry4eMQ8P34n0-QatwrkGUfLOHrE=
